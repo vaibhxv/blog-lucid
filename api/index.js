f1e3310f1521707cfeca14dd1d5bcc034f594038
Vaibhav Rohaan <82140153+vaibhxv@users.noreply.github.com>
2024-06-02 14:08:28 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-var cors = require('cors');
+import cors from 'cors';
 dotenv.config();
 
 mongoose
