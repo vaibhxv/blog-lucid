7a255f44d077baa0873d235c00d4356a7b0246d7
Vaibhav Rohaan <82140153+vaibhxv@users.noreply.github.com>
2024-06-02 13:09:25 +0530

    Update DashboardComp.jsx

--- client/src/components/DashboardComp.jsx
+++ client/src/components/DashboardComp.jsx
@@ -17,7 +17,7 @@ export default function DashboardComp() {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await fetch('/api/user/getusers?limit=5');
+        const res = await fetch('https://blog-lucid.onrender.com/api/user/getusers?limit=5');
         const data = await res.json();
         if (res.ok) {
           setUsers(data.users);
@@ -30,7 +30,7 @@ export default function DashboardComp() {
     };
     const fetchPosts = async () => {
       try {
-        const res = await fetch('/api/post/getposts?limit=5');
+        const res = await fetch('https://blog-lucid.onrender.com/api/post/getposts?limit=5');
         const data = await res.json();
         if (res.ok) {
           setPosts(data.posts);
@@ -43,7 +43,7 @@ export default function DashboardComp() {
     };
     const fetchComments = async () => {
       try {
-        const res = await fetch('/api/comment/getcomments?limit=5');
+        const res = await fetch('https://blog-lucid.onrender.com/api/comment/getcomments?limit=5');
         const data = await res.json();
         if (res.ok) {
           setComments(data.comments);
