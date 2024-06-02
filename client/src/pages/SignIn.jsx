22cd2d1a95f02cd7bf249b20c7564ca77b85ad05
Vaibhav Rohaan <82140153+vaibhxv@users.noreply.github.com>
2024-06-02 13:13:57 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -24,7 +24,7 @@ export default function SignIn() {
     }
     try {
       dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
+      const res = await fetch('https://blog-lucid.onrender.com/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
